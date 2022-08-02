fd629d6975bd8a0b40247f850449bf32b564a7cd
narusighn <80968987+narusighn@users.noreply.github.com>
2022-08-02 21:19:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-video="";
-Status="";
-valueInput="";
-
-
-function preload(){}
+///Variable////
+wristR_x="";
+wristR_y="";
+wristR_score="";
 
 function setup(){
-    canvas=createCanvas(480,380);
-    canvas.center();
-    video=createVideo(VIDEO);
-    video.hide();
-}
-
-function detect(){
-    objectDetection=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML="Status: Detection Started";
-    valueInput=document.getElementById("inputOP").value;
+    ////canvas///
+    cnv=createCanvas(800,600);
+    cnv.parent("Canvas");
+    ///webcam///
+    cam=createCapture(VIDEO);
+    cam.size(800,600);
+    cam.hide();
+    ///intialise model///
+    pose=ml5.poseNet(cam,modelLoaded);
+    pose.on('pose',Outcomes);
 }
 
 function modelLoaded(){
-    console.log("Model is loaded");
-    Status=true;
-    
+    console.log("Elon Musks says AI robots are dangerous!!!!! Please listen to the Tesla guy");
+}
+
+///gotPoses function///
+function Outcomes(results){
+    if(results.length>0){
+        console.log(results);
+        wristR_x=results[0].pose.rightWrist.x;
+        wristR_y=results[0].pose.rightWrist.y;
+        wristR_score=results[0].pose.rightWrist.score;
+    }
 }
 
 function draw(){
-    image(video,0,0,480,380);
+    image(cam,0,0,800,550);
+    if(wristR_score>0.2){
+        fill(255,0,0);
+        stroke("black");
+        circle(35,wristR_x,wristR_y);
+    }
+
 }
